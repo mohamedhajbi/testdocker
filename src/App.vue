858e4421bf4aaad6501f9d302e62a86dae83ef6b
Mohamed Hajbi <hajbi662@gmail.com>
2023-07-17 10:29:03 +0000

    axios

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
-<script setup>
-
-</script>
-
-
-
 <template>
-    <section>
-        <div class="contaienr">
-            <h1>
-                hello!
-            </h1>
-        </div>
-    </section>
-</template>
+    <div>
+      <button @click="getData">Get Product</button>
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          {{ product.title }} - {{ product.price }}
+        </li>
+      </ul>
+      <p v-if="error">{{ error }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        products: [],
+        error: null,
+      };
+    },
+    methods: {
+      async getData() {
+        try {
+          const response = await axios.get('https://3000-mohamedhajbi-testdocker-h6ubmbuuzne.ws-eu101.gitpod.io/products');
+          this.products = response.data;
+        } catch (error) {
+          this.error = error.message;
+        }
+      },
+    },
+  };
+  </script>
+  
