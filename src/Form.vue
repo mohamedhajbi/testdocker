74fc910cf2652399101ca2375e714e7b53ea514e
hajbi hajbi <mohamed.hajbi.dev@gmail.com>
2023-07-21 15:03:07 +0000

    mt-2

--- src/Form.vue
+++ src/Form.vue
@@ -12,7 +12,7 @@
         <label for="price">Price</label>
         <input v-model="newProduct.price" type="number" class="form-control" id="price">
       </div>
-      <button @click="addProduct" class="btn btn-success">Add Product</button>
+      <button @click="addProduct" class="btn btn-success mt-2">Add Product</button>
     </div>
   </template>
   
@@ -35,11 +35,7 @@
         try {
           const response = await service.products.productsControllerAddProduct(this.newProduct);
           console.log("response", response);
-          // Assuming the API returns the newly added product in the response,
-          // you can add it to the local list of products for display
-          this.products.push(response.data); // Assuming you have "products" defined in the parent component (e.g., Products.vue)
-  
-          // Clear the form fields after successful addition
+          this.products.push(response.data); 
           this.newProduct.title = "";
           this.newProduct.description = "";
           this.newProduct.price = 0;
